b7910d21844d1eadbe3c2bb78cce225379829a1e
reedislost <reed@lostfr8.com>
2022-11-14 10:34:24 -0700

    added splitbee

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
+import splitbee from "@splitbee/web";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: "/bee.js",
+      apiUrl: "/_hive",
+    });
+  }, []);
   return (
     <ChakraProvider>
       <Component {...pageProps} />
